40948f7bebfa27ad9dcbf723573b7c97a6b47b53
Taylor Pelsue <taylorpelsue@gmail.com>
2018-02-18 18:39:58 -0600

    can fight at this point and reset. No hp message yet

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -1,83 +1,153 @@
-var goku;
-var vegeta;
-var piccolo;
-var frieza;
-var isCharacterChosen = false;
+var chosenFighter;
+var enemyFighter;
+var fighters = {
+    'goku': {
+        id: "gokufight",
+        name: 'Goku',
+        hp: 148,
+        power: 10,
+        increment: 5,
+        image: 'goku.png'
+    },
+    'vegeta': {
+        id: "vegetafight",
+        name: 'Vegeta',
+        hp: 145,
+        power: 10,
+        increment: 4,
+        image: 'vegeta.png'
+    },
+    'piccolo': {
+        id: "piccolofight",
+        name: 'Piccolo',
+        hp: 137,
+        power: 8,
+        increment: 6,
+        image: 'piccolo.png'
+    },
+    'frieza': {
+        id: "friezafight",
+        name: 'Frieza',
+        hp: 150,
+        power: 12,
+        increment: 3,
+        image: 'frieza.png'
+    }
+}
 
 $(document).ready(function () {
 
+    $("#startover").hide();
+    displayFighter(fighters['goku']);
+    displayFighter(fighters['vegeta']);
+    displayFighter(fighters['piccolo']);
+    displayFighter(fighters['frieza']);
+
     $("#gokufight").on("click", function () {
         $("#gokufight").appendTo("#you");
-        isCharacterChosen = true;
-        fighterChosen("goku");
-        
-        
+        chosenFighter = fighters['goku'];
+        fighterChosen();
     });
     $("#vegetafight").on("click", function () {
         $("#vegetafight").appendTo("#you");
-        isCharacterChosen = true;
-        fighterChosen("vegeta");
-       
-
+        chosenFighter = fighters['vegeta'];
+        fighterChosen();
     });
     $("#piccolofight").on("click", function () {
         $("#piccolofight").appendTo("#you");
-        isCharacterChosen = true;
-        fighterChosen("piccolo");
-        
+        chosenFighter = fighters['piccolo'];
+        fighterChosen();
     });
     $("#friezafight").on("click", function () {
         $("#friezafight").appendTo("#you");
-        isCharacterChosen = true;
-        fighterChosen("frieza");
-        
+        chosenFighter = fighters['frieza'];
+        fighterChosen();
     });
 });
 
-function fighterChosen(characterChosen){
-    if (characterChosen === "goku") {
+function fighterChosen() {
+    if (chosenFighter.id === "gokufight") {
         $("#vegetafight").appendTo("#enemy1").off();
         $("#piccolofight").appendTo("#enemy2").off();
         $("#friezafight").appendTo("#enemy3").off();
-    }else if(characterChosen === "vegeta") {
+    } else if (chosenFighter.id === "vegetafight") {
         $("#gokufight").appendTo("#enemy1").off();
         $("#piccolofight").appendTo("#enemy2").off();
         $("#friezafight").appendTo("#enemy3").off();
-    }else if(characterChosen === "piccolo") {
+    } else if (chosenFighter.id === "piccolofight") {
         $("#gokufight").appendTo("#enemy1").off();
         $("#vegetafight").appendTo("#enemy2").off();
         $("#friezafight").appendTo("#enemy3").off();
-    }else if(characterChosen === "frieza") {
+    } else if (chosenFighter.id === "friezafight") {
         $("#gokufight").appendTo("#enemy1").off();
         $("#vegetafight").appendTo("#enemy2").off();
         $("#piccolofight").appendTo("#enemy3").off();
     }
 
     $("#gokufight").on("click", function () {
-        $("#gokufight").appendTo("#defense");  
-        
+        $("#gokufight").appendTo("#defense");
+        enemyFighter = fighters['goku'];
     });
     $("#vegetafight").on("click", function () {
         $("#vegetafight").appendTo("#defense");
-
+        enemyFighter = fighters['vegeta'];
     });
     $("#piccolofight").on("click", function () {
         $("#piccolofight").appendTo("#defense");
-        
+        enemyFighter = fighters['piccolo'];
     });
     $("#friezafight").on("click", function () {
         $("#friezafight").appendTo("#defense");
+        enemyFighter = fighters['frieza'];
+    });
+
+    $("#attack").click(function () {
+        chosenFighter.hp = chosenFighter.hp - enemyFighter.power;
+        enemyFighter.hp = enemyFighter.hp - chosenFighter.power;
+        chosenFighter.power = chosenFighter.power + chosenFighter.increment;
+        redisplayFighter(chosenFighter);
+        redisplayFighter(enemyFighter);
+
+        if (enemyFighter.hp <= 0) {
+            $("#" + enemyFighter.id).hide();
+        };
+        if (chosenFighter.hp <= 0) {
+
+            $("#vs").html("Game Over! Try again?");
+            $("#attack").hide();
+            $("#startover").show();
+        }
+        $("#startover").click(function () {
+            location.reload();
+        });
 
     });
 }
 
+function displayFighter(fighter) {
+    var fighterDiv = "<div class='col-md-3'>" +
+        "<div class='fighter' id='" + fighter.id + "'>" +
+        "<p class='name'>" + fighter.name + "</p>" +
+        "<p class='hp'>" + fighter.hp + "</p>" +
+        "<img class='fighter-image' src='assets/images/" + fighter.image + "'>" +
+        "</div>" +
+        "</div>";
+    $("#fighters").append(fighterDiv);
+}
 
+function redisplayFighter(fighter) {
+    var fighterDiv = "<div class='fighter' id='" + fighter.id + "'>" +
+        "<p class='name'>" + fighter.name + "</p>" +
+        "<p class='hp'>" + fighter.hp + "</p>" +
+        "<img class='fighter-image' src='assets/images/" + fighter.image + "'>" +
+        "</div>";
+    $("#" + fighter.id).replaceWith(fighterDiv)
+}
 
-
-// if (!isOperatorClicked) {
-    //     //build x value
-    //     x += a;
-    //     $("#first-number").text(x);
-    // }
+// FIGHTING POWER; ATTACK POWER GAINED PER ATTACK
+//Frieza: 25; 8
+//Goku: 20; 10
+//Piccolo: 16; 12
+//Vegeta: 20; 8
 
 
